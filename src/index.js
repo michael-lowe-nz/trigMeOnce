ddd21e857b247b5f5edb14e478130d864d410655
michael-lowe-nz <mikkeyfluttie@gmail.com>
2017-04-14 11:31:09 +1200

    Change root in app.js, add some comoponents

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createStore } from 'redux'
 import './index.css';
+
+const root = document.getElementById('root')
 
 ReactDOM.render(
   <App />,
-  document.getElementById('root')
+  root
 );
