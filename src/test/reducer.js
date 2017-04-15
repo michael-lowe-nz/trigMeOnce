1af167c385895ed5ad32e0526cff101d167a191c
michael-lowe-nz <mikkeyfluttie@gmail.com>
2017-04-15 15:04:56 +1200

    Start to add tests for the reducer

--- src/test/reducer.js
+++ src/test/reducer.js
@@ -1,6 +1,31 @@
 import test from 'tape'
+import freeze from 'deep-freeze'
+import reducer from '../reducer'
 
-test('passing test', (t) => {
+test('passing test', t => {
   t.pass()
   t.end()
+})
+
+test('Test that reducer can receive triangle lengths', t => {
+  //ARRANGE
+  const initialState = {
+    triangle: {
+      1: null,
+      2: null,
+      3: null,
+    }
+  }
+  freeze(initialState)
+  const expected = {
+    triangle: {
+      1: 25,
+      2: null,
+      3: null
+    }
+  }
+  //ACT
+
+  const actual = reducer(initialState, {type: 'ADD_ANGLE', payload: [1, 25]})
+  //ASSERT
 })
