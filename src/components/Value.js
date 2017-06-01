5f843df4ae409dd98102212cd62aa696f1b8dabb
michael-lowe-nz <mikkeyfluttie@gmail.com>
2017-06-02 08:32:48 +1200

    Remove console.logs

--- src/components/Value.js
+++ src/components/Value.js
@@ -3,7 +3,6 @@ import InlineEdit from 'react-edit-inline'
 
 module.exports = ({ state, dispatch, sideType }) => {
   function handleChange(data) {
-    console.log("HandlingChange")
     dispatch({
       type: 'ADD_LENGTH',
       payload: {side: sideType, length: data.value}
