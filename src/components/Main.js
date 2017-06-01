17b3d90fbb2de043748b40c16a8bd26095036791
michael-lowe-nz <mikkeyfluttie@gmail.com>
2017-06-02 08:26:46 +1200

    Tidy up on main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,6 +8,6 @@ import About from './About'
 module.exports = ({ state, dispatch, sideType }) =>
   <Switch>
     <Route exact path="/" component={Welcome}/>
-    <Route path="/calculator" render={ () => <Calculator state={state} dispatch={dispatch}/>}/>
+    <Route path="/calculator" render={() => <Calculator state={state} dispatch={dispatch}/>}/>
     <Route path="/about" component={About}/>
   </Switch>
